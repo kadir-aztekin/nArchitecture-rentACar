2a7610c9e6dc2100dd32a019090c2918c00dc754
KADİR AZTEKİN <76233680+kadir-aztekin@users.noreply.github.com>
2022-08-22 23:46:03 +0300

    Program cs ExceptionMiddleware Update

--- src/demoProjects/rentACar/WebAPI/Program.cs
+++ src/demoProjects/rentACar/WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using Application;
+using Core.CrossCuttingConcerns.Exceptions;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+if (app.Environment.IsProduction())
+    app.ConfigureCustomExceptionMiddleware();
 
 app.UseAuthorization();
 
