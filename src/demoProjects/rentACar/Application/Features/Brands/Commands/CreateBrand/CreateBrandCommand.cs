0f6b2ac13d06029a7256f24b58401369d581403f
KADİR AZTEKİN <76233680+kadir-aztekin@users.noreply.github.com>
2022-08-22 23:38:51 +0300

    Rules & Controller added / CQRS Update

--- src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Features.Brands.Dtos;
+using Application.Features.Brands.Rules;
 using Application.Services.Repositories;
 using AutoMapper;
 using Domain.Entites;
@@ -14,24 +15,30 @@ namespace Application.Features.Brands.Commands.CreateBrand
     public class CreateBrandCommand:IRequest<CreatedBrandDto>
     {
         public string Name { get; set; }
+        
         public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandDto>
         {
             private readonly IBrandRepository _brandRepository;
             private readonly IMapper _mapper;
+            private readonly BrandBusinessRules _brandBusinessRules;
 
             //Constractor burası asagısı aslında sınıfın kodları
-            public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+            public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper, BrandBusinessRules brandBusinessRules)
             {
                 _brandRepository = brandRepository;
                 _mapper = mapper;
+                _brandBusinessRules = brandBusinessRules;
             }
 
             //TASK OLMASI HANDLE FONK IREQUESTEN GELIYOR
             public async Task<CreatedBrandDto> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
             {
+                await _brandBusinessRules.BrandNameCanNotBeDuplicatedWhenInserted(request.Name);
+
                 Brand mappedBrand = _mapper.Map<Brand>(request);
                 Brand createdBrand = await _brandRepository.AddAsync(mappedBrand);
                 CreatedBrandDto createdBrandDto = _mapper.Map<CreatedBrandDto>(createdBrand);
+
                 return createdBrandDto;
 
             }
