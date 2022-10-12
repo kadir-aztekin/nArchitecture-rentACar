1b40037fdbbbf954bdd1669eedf195e2f8066374
kadir-aztekin <aztekadir@icloud.com>
2022-10-13 00:57:54 +0300

    jwt update

--- src/demoProjects/rentACar/Application/Features/Auths/Commands/Register/RegisterCommand.cs
+++ src/demoProjects/rentACar/Application/Features/Auths/Commands/Register/RegisterCommand.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Features.Auths.Dtos;
+using Application.Features.Auths.Rules;
+using Application.Services.Repositories;
 using Core.Security.Dtos;
 using MediatR;
 using System;
@@ -16,6 +18,8 @@ namespace Application.Features.Auths.Commands.Register
 
         public class RegisterCommandHandler : IRequestHandler<RegisterCommand, RegisterDto>
         {
+            private readonly AuthBusinessRules _authBusinessRules;
+            private readonly IUserRepository _userRepository;
             public Task<RegisterDto> Handle(RegisterCommand request, CancellationToken cancellationToken)
             {
                 throw new NotImplementedException();
---
+++ src/demoProjects/rentACar/Persistence/Repositories/UserOperationClaimRepository.cs
@@ -0,0 +1,14 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Core.Security.Entities;
+using Persistence.Context;
+
+namespace Persistence.Repositories
+{
+    public class UserOperationClaimRepository : EfRepositoryBase<UserOperationClaim, BaseDbContext>, IUserOperationClaimRepository
+    {
+        public UserOperationClaimRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
